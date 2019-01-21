b3ae4a1227bc0ef8c0241222a1d7f774fcf3caab
Beston <beston@modiface.com>
2019-01-21 15:41:43 -0500

    updated

--- Public/Scripts/Accept_script.js
+++ Public/Scripts/Accept_script.js
@@ -1,13 +1,12 @@
 // @input Component.Label label
 // @input Component.Label left
 // @input Component.Label right
-// @input Component.SpriteVisual countDownSprite
+
 
 global.startCountDown();
 script.left.outlineColor = new vec4(1.0, 0.0, 0, 1.0);
 script.left.outlineSize = 1.0;
 script.right.outlineSize = 0.0;
 script.label.enabled = true;
-script.countDownSprite.enabled = true;
 script.label.text = "accepted";
 print("accepted");
